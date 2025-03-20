68b9e94464f423f69ded667832d8b4f80e449558
Pitstopgamezone <pitstoplounge29@gmail.com>
2025-03-20 11:17:34 +0200

    ok

--- public/js/service-script.js
+++ public/js/service-script.js
@@ -129,3 +129,34 @@ languageSwitcher.addEventListener('click', function() {
     window.location.href = newFile;
 });
 
+
+// Get the modal
+var modal = document.getElementById("privacy-policy-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("privacy-policy-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    fetch('documents/privacy-policy-bike.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('privacy-policy-text').textContent = data;
+            modal.style.display = "block";
+        });
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
